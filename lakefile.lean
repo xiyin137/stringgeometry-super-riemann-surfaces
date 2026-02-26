617976775a894ff0c8638194f7891f561c269442
Xi Yin <xiyin137@gmail.com>
2026-02-25 23:26:24 -0500

    Bump supermanifolds dependency to latest quality-gated commit

--- lakefile.lean
+++ lakefile.lean
@@ -7,7 +7,7 @@ require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "master"
 
 require SGSupermanifolds from git
-  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "fc8efce"
+  "https://github.com/xiyin137/stringgeometry-supermanifolds.git" @ "121ab54"
 
 require SGRiemannSurfaces from git
   "https://github.com/xiyin137/stringgeometry-riemann-surfaces.git" @ "62a9791"
